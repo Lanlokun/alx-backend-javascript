03cc6e203fdb6094d709167bbfd2cbeb4bcc3cc3
malik <mlanlokun@gmail.com>
2023-06-29 23:59:35 +0300

    made functions importable

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,4 +1,4 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
     promise
       .then((response) => {
         console.log("Got a response from the API");
