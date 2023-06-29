2386d1b6bfad2d22b13299f9b6d0e06c49a4118c
malik <mlanlokun@gmail.com>
2023-06-30 00:35:57 +0300

    Handle multiple successful promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -5,7 +5,6 @@ export default function handleResponseFromAPI(promise) {
         return { status: 200, body: "success" };
       })
       .catch((error) => {
-        console.log("Got a response from the API");
         return new Error();
       });
   }
