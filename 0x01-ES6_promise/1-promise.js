7dc785f0f66ce71a9658680ae21901cfea1f3162
malik <mlanlokun@gmail.com>
2023-06-29 22:11:46 +0300

    update

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,20 +1,9 @@
-function getFullResponseFromAPI(success)
-{
-    if(success)
-    {
-        return Promise.resolve(
-            {
-                status: 200,
-                body: 'Success'
-            }
-        );
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: "Success" });
+    } else {
+      reject(new Error("The fake API is not working currently"));
     }
-    
-    else
-    {
-    
-        return Promise.reject(
-            new Error('The fake API is not working currently')
-        );
-    }    
+  });
 }
