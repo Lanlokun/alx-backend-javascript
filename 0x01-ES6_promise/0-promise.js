7beba42102fa1f29e891d017a1856f4c3c7232fd
malik <mlanlokun@gmail.com>
2023-06-29 22:01:45 +0300

    Return a Promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,17 @@
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      // Simulating an asynchronous API call
+      setTimeout(() => {
+        const response = "API response data";
+        // Check if the response was successful or not
+        const isSuccess = true;
+  
+        if (isSuccess) {
+          resolve(response); // Resolve the Promise with the response data
+        } else {
+          reject("Error: Failed to fetch data from API"); // Reject the Promise with an error message
+        }
+      }, 2000); // Simulating a delay of 2000 milliseconds (2 seconds)
+    });
+  }
+  
