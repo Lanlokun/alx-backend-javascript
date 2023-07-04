cce3e324bc11628405700f40688dd0dd604de060
malik <mlanlokun@gmail.com>
2023-07-04 13:36:07 +0300

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
-export default function cleanSet(setInput, startString) {
+export default function cleanSet(set, startString) {
   if (typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
   const startStringLen = startString.length;
-  return [...setInput].filter((element) => element.startsWith(startString)).map((element) => element.slice(startStringLen)).join('-');
+  return [...set].filter((element) => element.startsWith(startString)).map((element) => element.slice(startStringLen)).join('-');
 }
