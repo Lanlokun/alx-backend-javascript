380d5f01f2febdd669feefa68675f591825e3860
malik <mlanlokun@gmail.com>
2023-07-06 21:42:24 +0300

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,4 @@
-interface ITeacher {
+export interface ITeacher {
     firstName: string;
     lastName: string;
     fullTimeEmployee: boolean;
@@ -7,15 +7,49 @@ interface ITeacher {
     [propName: string]: any;
 }
 
-interface IDirectors extends ITeacher {
+export interface IDirectors extends ITeacher {
     numberOfReports: number;
 }
 
-interface IprintTeacherFunction {
+export interface IprintTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
 
-export default function printTeacher(firstName: string, lastName: string): string {
+export function printTeacher(firstName: string, lastName: string): string {
     return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+
+export interface IStudentConstructor {
+    new(firstName: string, lastName: string): IStudentClass;
+}
+
+export interface IStudentClass {
+    firstName: string;
+    lastName: string;
+}
+
+
+export class StudentClass implements IStudentClass {
+
+    constructor(public firstName: string, public lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework() {
+        return 'Currently working';
+    }
+
+    displayName() {
+        return this.firstName;
+    }
+}
+
+
+
+
+
+
+
