653f8df15b5681c9abb019ab952ae6558007c6b6
malik <mlanlokun@gmail.com>
2023-08-26 03:02:05 +0300

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Malik Lanlokun <https://github.com/lanlokun>
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+  
+module.exports = displayMessage;
